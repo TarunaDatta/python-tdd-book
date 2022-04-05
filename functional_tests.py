88bbf6ab1e23b4981e3f36cd0fbee2367184695f
tarunadatta <tchittajallu@gmail.com>
2022-04-05 18:55:31 +0530

    Refactored functionaltests to add check_for_row_in_list_table method

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard of a new to-do app. She checks it out in the browser
         self.browser.get('http://localhost:8000')
@@ -30,11 +35,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as an item in a todo list
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
-        
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. 
         # She enters "Use peacock feathers to create a fly"
@@ -43,15 +44,9 @@ class NewVisitorTest(unittest.TestCase):
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn("2: Use peacock feathers to create a fly", 
-        [row.text for row in rows]
-        )
-        
-
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table("2: Use peacock feathers to create a fly")
+    
         # Edith wonders if the list is persistent. She then notices a unique URL 
         # has been created for her -- explanatory text to that effect
         self.fail("Finish the test")
