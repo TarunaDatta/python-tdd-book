c4a1ab0aa2cf002f7a556e4f03ba465a6267bb74
tarunadatta <tchittajallu@gmail.com>
2022-04-07 16:27:04 +0530

    Added a new list view URL, template and url path

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,4 @@
+from tkinter import EW
 from django.shortcuts import render, redirect
 from .models import Item
 from django.http import HttpResponse
@@ -5,9 +6,10 @@ from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
     if request.method == 'POST':
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-        return redirect("/")
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect("/lists/the-only-list-in-the-world/")
+    return render(request, 'home.html')
 
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html', {'items': items})
+    return render(request, 'list.html', {'items': items})
