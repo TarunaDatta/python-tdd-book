b44090d9c89a3a3b3cbf696bd6a1957028a45687
tarunadatta <tchittajallu@gmail.com>
2022-04-10 15:38:52 +0530

    Adjusted new_list to do model validation to catch empty list items

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,8 +1,6 @@
 from hashlib import new
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
-from lists.views import home_page
+from django.utils.html import escape
 from lists.models import Item, List
 # Create your tests here.
 
@@ -81,3 +79,15 @@ class NewItemTest(TestCase):
         )
 
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+
+    def test_invalid_list_items_are_not_saved(self):
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
