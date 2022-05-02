15fcdc772d8daf50c0bda95d23b050fe1ea08488
tarunadatta <tchittajallu@gmail.com>
2022-05-02 12:10:52 +0530

    new_list view tries to assign owner but cannot

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -18,18 +18,14 @@ class NewVisitorTest(FunctionalTest):
         self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
 
         # She types "Buy peacock feathers" into a text box 
-        input_box.send_keys('Buy peacock feathers')
         # When she hits enter, the page updates and now the page lists 
         # "1: Buy peacock feathers" as an item in a todo list
-        input_box.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.add_list_item("Buy peacock feathers")
 
         # There is still a text box inviting her to add another item. 
         # She enters "Use peacock feathers to create a fly"
-        input_box = self.get_item_input_box()
-        input_box.send_keys("Use peacock feathers to create a fly")
-        input_box.send_keys(Keys.ENTER)
-
+        self.add_list_item("Use peacock feathers to create a fly")
+        
         # The page updates again and shows both items in her list
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table("2: Use peacock feathers to create a fly")
@@ -39,10 +35,7 @@ class NewVisitorTest(FunctionalTest):
     def test_multiple_users_can_start_lists_at_different_urls(self):
         # Edith starts a new todo list
         self.browser.get(self.live_server_url)
-        inputbox = self.get_item_input_box()
-        inputbox.send_keys("Buy peacock feathers")
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.add_list_item("Buy peacock feathers")
 
         # She notices her list has a unique URL
         edith_list_url = self.browser.current_url
@@ -63,10 +56,7 @@ class NewVisitorTest(FunctionalTest):
 
         # Francis starts a new list by entering a new item. He is
         # less interesting than Edith
-        inputbox = self.get_item_input_box()
-        inputbox.send_keys("Buy milk")
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table("1: Buy milk")
+        self.add_list_item("Buy milk")
 
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
