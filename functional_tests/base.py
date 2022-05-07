6437a494ed3dd1a2ee3112ffcfb7e7ada312c2e9
tarunadatta <tchittajallu@gmail.com>
2022-05-07 11:56:32 +0530

    Create page objects for list sharing, moved the wait decorator definition, extended the test_sharing fts

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -18,6 +18,21 @@ SCREEN_DUMP_LOCATION = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), "screendumps"
 )
 
+
+def wait(fn):
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time >= MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -77,20 +92,6 @@ class FunctionalTest(StaticLiveServerTestCase):
             value=session_key,
             path="/",
         ))
-
-
-    def wait(fn):
-        def modified_fn(*args, **kwargs):
-            start_time = time.time()
-            while True:
-                try:
-                    return fn(*args, **kwargs)
-
-                except (AssertionError, WebDriverException) as e:
-                    if time.time() - start_time >= MAX_WAIT:
-                        raise e
-                    time.sleep(0.5)
-        return modified_fn
 
     @wait
     def wait_for_row_in_list_table(self, row_text):
